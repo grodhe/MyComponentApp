4bf169c0a89ded3b4edd785a666a1982d52aceb9
grodhe <gert.rodhelind@telia.com>
2026-08-07 20:02:58 +0200

    Linux build!

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,16 @@
-#docker run --rm -p 8080:80 nginx:alpine
-#mkdir data
-#mkdir logs
-#cd logs/
-#mkdir backend
+if [ "$1" = "-f" ]
+then
+   sudo docker run --rm -p 8080:80 nginx:alpine
+   mkdir data
+   mkdir logs
+   cd logs/
+   mkdir backend
+elif [ "$1" = "-help" ]
+then
+    echo "First tim use -f as a parameter!"
+else
+        echo "Not"
+fi
 sudo docker compose down
 sudo docker compose build
 sudo docker compose up -d
