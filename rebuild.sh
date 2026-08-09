7b0b8c7f3ef573d889bdea7fa76090d09838d368
grodhe <gert.rodhelind@telia.com>
2026-08-09 20:15:08 +0200

    docker_compose.yml softlink!

--- rebuild.sh
+++ rebuild.sh
@@ -7,6 +7,7 @@ then
    mkdir backend
    cd /volume1/MyDockerImages/MyComponentApp
    cp   /volume1/MyDockerImages/bck/.env  /volume1/MyDockerImages/MyComponentApp/.env
+   ln -s /volume1/MyDockerImages/MyComponentApp/docker-compose-prod.yml /volume1/MyDockerImages/MyComponentApp/docker-compose.yml
 elif [ "$1" = "-help" ]
 then
     echo "First tim use -f as a parameter!"
